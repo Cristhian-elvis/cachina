4c371d9761c61d7f9b243a5d98eea313cc4a3a93
Cristhian <cristhianvv2@gmail.com>
2021-05-14 08:10:39 -0500

    conf1.1

--- anunciosCachina/settings.py
+++ anunciosCachina/settings.py
@@ -139,12 +139,12 @@ USE_TZ = True
 
 #MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
